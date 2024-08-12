56b326522457657d7b8d5a95dc1da5012f9964e8
Salman <salmanhn719@gmail.com>
2024-08-12 18:12:12 +1000

    Input element

---
+++ components/formElements/input.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./input.module.css";
+import classNames from "classnames";
+
+interface PropType extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+}
+
+const Input: React.FC<PropType> = ({ label, error, ...rest }: PropType) => {
+  return (
+    <div className={styles.container}>
+      {label && <label className={styles.label}>{label}</label>}
+      <input
+        {...rest}
+        className={classNames(
+          styles.input,
+          rest.className,
+          error && styles["error-input"],
+        )}
+      />
+      {error && <div className={styles.error}>{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
